0e96e1c71ec84e9f5e2c8a7b3b775a11cee068b9
Nishant Singhal <nishantsinghal900@gmail.com>
2022-04-02 19:53:55 +0530

    push notification

---
+++ lib/services/pushNotification_service.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: deprecated_member_use
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+
+//var serverUrl = dotenv.env['SERVER_URL'];
+
+Future<PushNotificationResponse> createPushNotification(
+    String id, String token) async {
+  String uuid = UniqueKey().toString();
+
+  var serverUrl; //later remove
+  final response = await http.post(
+    Uri.parse(serverUrl! + 'push-notification'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'uuid': uuid,
+      'token': token,
+      'platform': 'android',
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    print(response.body);
+    return PushNotificationResponse.fromJson(jsonDecode(response.body));
+  } else if (response.statusCode == 422) {
+    throw ('Unable to push notification');
+  } else {
+    throw Exception('Network Error');
+  }
+}
+
+class PushNotificationResponse {
+  final String title;
+  final String description;
+  final DateTime expiredAt;
+
+  PushNotificationResponse(
+      {required this.title,
+      required this.description,
+      required this.expiredAt});
+
+  factory PushNotificationResponse.fromJson(Map<String, dynamic> json) {
+    return PushNotificationResponse(
+      title: json['title'],
+      description: json['description'],
+      expiredAt: json['expiredAt'],
+    );
+  }
+}
