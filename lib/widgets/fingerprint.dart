ce8578997d87ed7f3df2560f8d99fa54ae4e5666
SK MABUD ALAM <pavelalam401@gmail.com>
2023-06-08 02:14:58 +0530

    ignore 'BuildContext's across async gaps

--- lib/widgets/fingerprint.dart
+++ lib/widgets/fingerprint.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:crypto/crypto.dart';
 import 'package:pem/pem.dart';
 
--- lib/widgets/taskdetails/dateTimePicker.dart
+++ lib/widgets/taskdetails/dateTimePicker.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
 
