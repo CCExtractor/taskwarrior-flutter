51cf39ba97942f3602dc4dd3d5f9510a91e8c38c
SK MABUD ALAM <pavelalam401@gmail.com>
2023-06-29 18:38:19 +0530

    App constant colors

---
+++ lib/config/taskwarriorcolors.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Appcolors {
+  static Color red = Colors.red;
+  static Color green = Colors.green;
+  static Color yellow = Colors.yellow;
+  static Color white = Colors.white;
+}
