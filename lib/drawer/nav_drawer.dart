3c9f118079b9fecd778db7a9d960bcccb9e2c8c0
Pavel Alam <pavelalam401@gmail.com>
2023-09-16 15:38:45 +0530

    redesigned the Nav drawer

--- lib/drawer/nav_drawer.dart
+++ lib/drawer/nav_drawer.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-
+import 'package:sizer/sizer.dart';
 import 'package:taskwarrior/config/app_settings.dart';
 import 'package:taskwarrior/model/storage/storage_widget.dart';
-import 'package:taskwarrior/routes/pageroute.dart';
 import 'package:taskwarrior/views/about/about.dart';
+import 'package:taskwarrior/views/profile/profile.dart';
 import 'package:taskwarrior/views/reports/reports_home.dart';
 import 'package:taskwarrior/config/theme_switcher_clipper.dart';
 import 'package:taskwarrior/views/settings/settings.dart';
@@ -29,129 +29,130 @@ class _NavDrawerState extends State<NavDrawer> {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: AppSettings.isDarkMode ? Colors.black : Colors.white,
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          ListTile(
-            tileColor: AppSettings.isDarkMode ? Colors.black : Colors.white,
-            textColor: AppSettings.isDarkMode ? Colors.white : Colors.black,
-            contentPadding: const EdgeInsets.only(top: 40, left: 10),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Menu',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: ThemeSwitcherClipper(
-                    isDarkMode: AppSettings.isDarkMode,
-                    onTap: (bool newMode) async {
-                      AppSettings.isDarkMode = newMode;
-                      setState(() {});
-                      await SelectedTheme.saveMode(AppSettings.isDarkMode);
-                      widget.notifyParent();
-                    },
-                    child: Icon(
-                      AppSettings.isDarkMode
-                          ? Icons.dark_mode
-                          : Icons.light_mode,
+      child: Container(
+        color: AppSettings.isDarkMode ? Colors.black : Colors.white,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              color: AppSettings.isDarkMode ? Colors.black : Colors.white,
+              padding: const EdgeInsets.only(top: 50, left: 15, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Menu',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
                       color:
                           AppSettings.isDarkMode ? Colors.white : Colors.black,
-                      size: 15,
                     ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: ThemeSwitcherClipper(
+                      isDarkMode: AppSettings.isDarkMode,
+                      onTap: (bool newMode) async {
+                        AppSettings.isDarkMode = newMode;
+                        setState(() {});
+                        await SelectedTheme.saveMode(AppSettings.isDarkMode);
+                        widget.notifyParent();
+                      },
+                      child: Icon(
+                        AppSettings.isDarkMode
+                            ? Icons.dark_mode
+                            : Icons.light_mode,
+                        color: AppSettings.isDarkMode
+                            ? Colors.white
+                            : Colors.black,
+                        size: 15,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            onTap: () async {
-              AppSettings.isDarkMode = !AppSettings.isDarkMode;
-              setState(() {});
-              await SelectedTheme.saveMode(AppSettings.isDarkMode);
-              widget.notifyParent();
-            },
-          ),
-          ListTile(
-            tileColor: AppSettings.isDarkMode ? Colors.black : Colors.white,
-            textColor: AppSettings.isDarkMode ? Colors.white : Colors.black,
-            leading: Icon(
-              Icons.person_rounded,
+            Container(
+              color: AppSettings.isDarkMode ? Colors.black : Colors.white,
+              height: 3.h,
+            ),
+            buildMenuItem(
+              icon: Icons.person_rounded,
+              text: 'Profile',
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const ProfilePage(),
+                  ),
+                );
+                // Navigator.pushNamed(context, PageRoutes.profile);
+              },
+            ),
+            buildMenuItem(
+              icon: Icons.summarize,
+              text: 'Reports',
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const ReportsHome(),
+                  ),
+                );
+              },
+            ),
+            buildMenuItem(
+              icon: Icons.info,
+              text: 'About',
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const AboutPage(),
+                  ),
+                );
+              },
+            ),
+            buildMenuItem(
+              icon: Icons.settings,
+              text: 'Settings',
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const SettingsPage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildMenuItem(
+      {required IconData icon,
+      required String text,
+      required VoidCallback onTap}) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        color: AppSettings.isDarkMode ? Colors.black : Colors.white,
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+        child: Row(
+          children: [
+            Icon(
+              icon,
               color: AppSettings.isDarkMode ? Colors.white : Colors.black,
             ),
-            title: const Text('Profile'),
-            onTap: () {
-              Navigator.pushNamed(context, PageRoutes.profile);
-            },
-          ),
-          // Uncomment the following ListTile if you want to enable the "Refresh" button
-          /*
-          ListTile(
-            tileColor: AppSettings.isDarkMode ? Colors.black : Colors.white,
-            textColor: AppSettings.isDarkMode ? Colors.white : Colors.black,
-            leading: Icon(
-              Icons.refresh,
-              color: AppSettings.isDarkMode ? Colors.white : Colors.black,
+            const SizedBox(width: 10),
+            Text(
+              text,
+              style: TextStyle(
+                color: AppSettings.isDarkMode ? Colors.white : Colors.black,
+                fontSize: 18,
+              ),
             ),
-            onTap: () {
-              Navigator.pop(context);
-              widget.storageWidget.synchronize(context, true);
-            },
-            title: const Text("Refresh"),
-          ),
-          */
-          ListTile(
-            tileColor: AppSettings.isDarkMode ? Colors.black : Colors.white,
-            textColor: AppSettings.isDarkMode ? Colors.white : Colors.black,
-            leading: Icon(
-              Icons.summarize,
-              color: AppSettings.isDarkMode ? Colors.white : Colors.black,
-            ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const ReportsHome(),
-                ),
-              );
-            },
-            title: const Text("Reports"),
-          ),
-          ListTile(
-            tileColor: AppSettings.isDarkMode ? Colors.black : Colors.white,
-            textColor: AppSettings.isDarkMode ? Colors.white : Colors.black,
-            leading: Icon(
-              Icons.info,
-              color: AppSettings.isDarkMode ? Colors.white : Colors.black,
-            ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const AboutPage(),
-                ),
-              );
-            },
-            title: const Text("About"),
-          ),
-          ListTile(
-            tileColor: AppSettings.isDarkMode ? Colors.black : Colors.white,
-            textColor: AppSettings.isDarkMode ? Colors.white : Colors.black,
-            leading: Icon(
-              Icons.settings,
-              color: AppSettings.isDarkMode ? Colors.white : Colors.black,
-            ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const SettingsPage(),
-                ),
-              );
-            },
-            title: const Text("Settings"),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
