3486f610e20d5e20f29d5e96e9f3c337742cc613
its-me-abhishek <bumblebee31304@gmail.com>
2023-11-16 16:15:54 +0530

    UI Bug fixes

--- lib/widgets/add_Task.dart
+++ lib/widgets/add_Task.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously, file_names
+
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
