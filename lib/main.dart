9f131a3dfe330f64db2ba52f821cb7bdc35faf9e
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-06-02 23:37:30 +0530

    Notifications working fine

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:taskwarrior/app/utils/theme/app_settings.dart';
 
 import 'app/routes/app_pages.dart';
@@ -8,6 +9,11 @@ import 'app/routes/app_pages.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppSettings.init();
+  await Permission.notification.isDenied.then((value) {
+    if (value) {
+      Permission.notification.request();
+    }
+  });
   runApp(
     GetMaterialApp(
       title: "Application",
