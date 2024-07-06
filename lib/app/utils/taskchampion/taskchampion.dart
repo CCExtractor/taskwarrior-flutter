1c2898f16b4bc8b9d662bc49fb78d6e2dfecc97c
Abhishek <bumblebee31304@gmail.com>
2024-07-06 18:08:36 +0530

    fix: minor bug fix

--- lib/app/utils/taskchampion/taskchampion.dart
+++ lib/app/utils/taskchampion/taskchampion.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
