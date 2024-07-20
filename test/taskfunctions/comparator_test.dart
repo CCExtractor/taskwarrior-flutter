08ac7c14f8951de0770b4a7c7515a04870bbf88b
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-07-20 11:00:06 +0530

    Tests added for comparator and query

---
+++ test/taskfunctions/comparator_test.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:test/test.dart';
+import 'package:taskwarrior/app/utils/taskfunctions/comparator.dart';
+import 'package:taskwarrior/app/models/json/task.dart';
+import 'package:built_collection/built_collection.dart';
+
+void main() {
+  final task1 = Task((builder) => builder
+    ..entry = DateTime(2024, 7, 20)
+    ..modified = DateTime(2024, 7, 21)
+    ..start = DateTime(2024, 7, 22)
+    ..due = DateTime(2024, 7, 23)
+    ..priority = 'H'
+    ..project = 'Project A'
+    ..tags = ListBuilder<String>(['tag1', 'tag2']));
+
+  final task2 = Task((builder) => builder
+    ..entry = DateTime(2024, 7, 19)
+    ..modified = DateTime(2024, 7, 18)
+    ..start = DateTime(2024, 7, 21)
+    ..due = DateTime(2024, 7, 22)
+    ..priority = 'L'
+    ..project = 'Project B'
+    ..tags = ListBuilder<String>(['tag1']));
+
+  test('Test compareTasks for Created column', () {
+    final compare = compareTasks('Created');
+    expect(compare(task1, task2), greaterThan(0));
+  });
+
+  test('Test compareTasks for Modified column', () {
+    final compare = compareTasks('Modified');
+    expect(compare(task1, task2), lessThan(0));
+  });
+
+  test('Test compareTasks for Start Time column', () {
+    final compare = compareTasks('Start Time');
+    expect(compare(task1, task2), greaterThan(0));
+  });
+
+  test('Test compareTasks for Priority column', () {
+    final compare = compareTasks('Priority');
+    expect(compare(task1, task2), lessThan(0));
+  });
+
+  test('Test compareTasks for Project column', () {
+    final compare = compareTasks('Project');
+    expect(compare(task1, task2), greaterThan(0));
+  });
+
+  test('Test compareTasks for Tags column', () {
+    final compare = compareTasks('Tags');
+    expect(compare(task1, task2), greaterThan(0));
+  });
+
+  test('Test compareTasks for Urgency column', () {
+    final compare = compareTasks('Urgency');
+    expect(compare(task1, task2), lessThan(0));
+  });
+}
