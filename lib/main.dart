b6fe7450cd0442e5f505c6fe36ca29a9f1ac5ab1
Chinmay Chaudhari <111864031+BrawlerXull@users.noreply.github.com>
2024-11-05 16:16:56 +0530

    Removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:taskwarrior/app/utils/app_settings/app_settings.dart';
 import 'package:taskwarrior/app/utils/permissions/permissions_manager.dart';
 import 'app/routes/app_pages.dart';
