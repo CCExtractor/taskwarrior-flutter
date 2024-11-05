63a35f70b732ba3a40851f08fc14d5bdaf767107
Shubham04jha <shubhamjha70491@gmail.com>
2024-11-05 12:04:27 +0530

    loading screen bug fixed, by giving permissions

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,34 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppSettings.init();
 
-  await Permission.notification.isDenied.then((value) {
-    if (value) {
-      Permission.notification.request();
+  // await Permission.notification.isDenied.then((value) {
+  //   if (value) {
+  //     Permission.notification.request();
+  //   }
+  // });
+  if (await Permission.notification.isDenied) {
+    if (await Permission.notification.request().isGranted) {
+      print("Notification permission granted");
+    } else {
+      print("Notification permission denied");
     }
-  });
+  }
+
+  if (await Permission.storage.isDenied) {
+    if (await Permission.storage.request().isGranted) {
+      print("Storage permission granted");
+    } else {
+      print("Storage permission denied");
+    }
+  }
+
+  if (await Permission.manageExternalStorage.isDenied) {
+    if (await Permission.manageExternalStorage.request().isGranted) {
+      print("Manage external storage permission granted");
+    } else {
+      print("Manage external storage permission denied");
+    }
+  }
   runApp(
     GetMaterialApp(
       title: "Application",
