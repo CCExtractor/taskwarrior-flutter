c9f59e5a42fa903ca49ce821122afa2685cfdd6d
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-11-05 15:57:52 +0530

    Import bug fix

--- lib/app/modules/about/controllers/about_controller.dart
+++ lib/app/modules/about/controllers/about_controller.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
+import 'package:taskwarrior/app/utils/app_settings/app_settings.dart';
 import 'package:taskwarrior/app/utils/language/supported_language.dart';
-import 'package:taskwarrior/app/utils/theme/app_settings.dart';
 
 class AboutController extends GetxController {
   final Rx<SupportedLanguage> selectedLanguage = SupportedLanguage.english.obs;
