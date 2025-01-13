5434792ae2ea978034754cfe0b94b4d61080be57
rohansen856 <rohansen856@gmail.com>
2025-01-13 21:13:34 +0530

    converted functional component to StatelessWidget

--- lib/app/modules/permission/views/permission_view.dart
+++ lib/app/modules/permission/views/permission_view.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:taskwarrior/app/modules/permission/views/permission_section.dart';
 import 'package:taskwarrior/app/utils/app_settings/app_settings.dart';
 import 'package:taskwarrior/app/utils/constants/taskwarrior_colors.dart';
 import '../controllers/permission_controller.dart';
@@ -51,8 +52,7 @@ class PermissionView extends GetView<PermissionController> {
                       : TaskWarriorColors.white,
                 ),
                 const SizedBox(height: 32),
-                _buildPermissionSection(
-                  context,
+                PermissionSection(
                   icon: Icons.folder_outlined,
                   title: 'Storage Permission',
                   description:
@@ -62,8 +62,7 @@ class PermissionView extends GetView<PermissionController> {
                   isDarkMode: isDarkMode,
                 ),
                 const SizedBox(height: 24),
-                _buildPermissionSection(
-                  context,
+                PermissionSection(
                   icon: Icons.notifications_outlined,
                   title: 'Notification Permission',
                   description:
@@ -137,60 +136,6 @@ class PermissionView extends GetView<PermissionController> {
             ),
           ),
         ),
-      ),
-    );
-  }
-
-  Widget _buildPermissionSection(
-    BuildContext context, {
-    required IconData icon,
-    required String title,
-    required String description,
-    required bool isDarkMode,
-  }) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: isDarkMode
-              ? TaskWarriorColors.ksecondaryBackgroundColor
-              : TaskWarriorColors.borderColor,
-        ),
-        borderRadius: BorderRadius.circular(12),
-        color: isDarkMode
-            ? TaskWarriorColors.kdialogBackGroundColor
-            : TaskWarriorColors.kLightDialogBackGroundColor,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Icon(icon, color: TaskWarriorColors.black),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Text(
-                  title,
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: isDarkMode
-                            ? TaskWarriorColors.kprimaryTextColor
-                            : TaskWarriorColors.kLightPrimaryTextColor,
-                      ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          Text(
-            description,
-            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                  color: isDarkMode
-                      ? TaskWarriorColors.ksecondaryTextColor
-                      : TaskWarriorColors.kLightSecondaryTextColor,
-                ),
-          ),
-        ],
       ),
     );
   }
