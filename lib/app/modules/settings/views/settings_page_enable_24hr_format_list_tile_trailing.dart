2935626f4ae6a272f0bea24e222c77cf1c26f0d1
Mastermind-sap <66560136+Mastermind-sap@users.noreply.github.com>
2025-03-30 01:29:05 +0530

    Fixed 24 hour app settings to reflect all over app

--- lib/app/modules/settings/views/settings_page_enable_24hr_format_list_tile_trailing.dart
+++ lib/app/modules/settings/views/settings_page_enable_24hr_format_list_tile_trailing.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:taskwarrior/app/modules/home/controllers/home_controller.dart';
+import 'package:taskwarrior/app/utils/app_settings/app_settings.dart';
 
 import '../controllers/settings_controller.dart';
-
 
 class SettingsPageEnable24hrFormatListTileTrailing extends StatelessWidget {
   final SettingsController controller;
@@ -16,13 +14,14 @@ class SettingsPageEnable24hrFormatListTileTrailing extends StatelessWidget {
   Widget build(BuildContext context) {
     return Obx(
       () => Switch(
-        value: controller.change24hr.value,
+        value: AppSettings.use24HourFormatRx.value,
         onChanged: (bool value) async {
-          controller.change24hr.value = value;
-
-          final SharedPreferences prefs = await SharedPreferences.getInstance();
-          await prefs.setBool('24hourformate', value);
-          Get.find<HomeController>().change24hr.value = value;
+          AppSettings.use24HourFormatRx.value = value;
+          AppSettings.saveSettings(
+            AppSettings.isDarkMode,
+            AppSettings.selectedLanguage,
+            value,
+          );
         },
       ),
     );
