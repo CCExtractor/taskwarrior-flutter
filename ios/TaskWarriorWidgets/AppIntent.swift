c4b6919e8c06695c72e3d4f523d110aa9d1d8e7f
Mastermind-sap <66560136+Mastermind-sap@users.noreply.github.com>
2025-04-08 20:46:28 +0530

    removed default config parameter

--- ios/TaskWarriorWidgets/AppIntent.swift
+++ ios/TaskWarriorWidgets/AppIntent.swift
@@ -1,18 +1,9 @@
-//
-//  AppIntent.swift
-//  TaskWarriorWidgets
-//
-//  Created by Saptarshi Adhikari on 08/04/25.
-//
-
 import WidgetKit
 import AppIntents
 
 struct ConfigurationAppIntent: WidgetConfigurationIntent {
     static var title: LocalizedStringResource { "Configuration" }
-    static var description: IntentDescription { "This is an example widget." }
+    // static var description: IntentDescription { "This is an example widget." }
 
     // An example configurable parameter.
-    @Parameter(title: "Favorite Emoji", default: "😃")
-    var favoriteEmoji: String
 }
