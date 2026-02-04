8c60251683b5a03f29dfec94281a4a8684f6820e
Prince <princebhadania007@gmail.com>
2026-02-04 13:53:53 +0530

    Solved CI

--- lib/app/models/filters.dart
+++ lib/app/models/filters.dart
@@ -1,13 +1,39 @@
+// lib/app/models/filters.dart
+
+class TagFilterMetadata {
+  final String display;
+  final bool selected;
+
+  TagFilterMetadata({
+    required this.display,
+    required this.selected,
+  });
+}
+
+class TagFilters {
+  final bool tagUnion;
+  final void Function() toggleTagUnion;
+  final Map<String, TagFilterMetadata> tags;
+  final void Function(String) toggleTagFilter;
+
+  TagFilters({
+    required this.tagUnion,
+    required this.toggleTagUnion,
+    required this.tags,
+    required this.toggleTagFilter,
+  });
+}
+
 class Filters {
   const Filters({
     required this.pendingFilter,
     required this.waitingFilter,
-    required this.completedFilter,  // NEW
-    required this.deletedFilter,    // NEW
+    required this.completedFilter,
+    required this.deletedFilter,
     required this.togglePendingFilter,
     required this.toggleWaitingFilter,
-    required this.toggleCompletedFilter,  // NEW
-    required this.toggleDeletedFilter,    // NEW
+    required this.toggleCompletedFilter,
+    required this.toggleDeletedFilter,
     required this.tagFilters,
     required this.projects,
     required this.projectFilter,
@@ -16,12 +42,12 @@ class Filters {
 
   final bool pendingFilter;
   final bool waitingFilter;
-  final bool completedFilter;  // NEW
-  final bool deletedFilter;    // NEW
+  final bool completedFilter;
+  final bool deletedFilter;
   final void Function() togglePendingFilter;
   final void Function() toggleWaitingFilter;
-  final void Function() toggleCompletedFilter;  // NEW
-  final void Function() toggleDeletedFilter;    // NEW
+  final void Function() toggleCompletedFilter;
+  final void Function() toggleDeletedFilter;
   final TagFilters tagFilters;
   final dynamic projects;
   final String projectFilter;
